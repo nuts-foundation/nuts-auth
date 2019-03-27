e05b629ecce81d6561aa345a64a268c37f98c661
stevenvegt <steven.vandervegt@nedap.com>
2019-03-27 10:13:53 +0100

    Add Docker and Skaffold config

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"github.com/nuts-foundation/nuts-proxy/cmd"
+	"github.com/sirupsen/logrus"
+	"os"
 )
 
-
 func main() {
+	logrus.SetOutput(os.Stdout)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		DisableColors: false,
+		ForceColors:   true,
+	})
 	cmd.Execute()
 }
