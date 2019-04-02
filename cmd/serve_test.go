1f5952f06eb40ad2f6b18621b8586f8d92fe53cc
stevenvegt <steven.vandervegt@nedap.com>
2019-04-02 16:01:03 +0200

    Add contract templating

--- cmd/serve_test.go
+++ cmd/serve_test.go
@@ -1,11 +1,12 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
 	"github.com/nuts-foundation/nuts-proxy/api"
 	"github.com/nuts-foundation/nuts-proxy/api/auth"
 	"github.com/privacybydesign/irmago"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -40,8 +41,51 @@ func TestContentType(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 }
 
+func TestCreateSessionHandlerUnknownType(t*testing.T){
+	sessionRequest := auth.ContractSigningRequest{ Type: "Unknown type", Language:"NL"}
+	jsonRequest, _ := json.Marshal(sessionRequest)
+	req, _ := http.NewRequest("POST", "/auth/contract/session",bytes.NewBuffer(jsonRequest))
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(auth.New().CreateSessionHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	status := rr.Code
+	if status != http.StatusBadRequest {
+		t.Errorf("Handler returned the wrong status: got %v, expected %v", status, http.StatusBadRequest)
+	}
+
+	message := string(rr.Body.Bytes())
+	if !strings.Contains(message, "Could not find contract with type Unknown type") {
+		t.Errorf("Expected different error message: %v", message)
+	}
+}
+
+func TestCreateSessionHandlerInvalidJson(t *testing.T) {
+	req, _ := http.NewRequest("POST", "/auth/contract/session",bytes.NewBuffer([]byte("foo:bar")))
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(auth.New().CreateSessionHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	status := rr.Code
+	if status != http.StatusBadRequest {
+		t.Errorf("Handler returned the wrong status: got %v, expected %v", status, http.StatusBadRequest)
+	}
+
+	message := string(rr.Body.Bytes())
+	if !strings.Contains(message, "Could not decode json request parameters") {
+		t.Errorf("Expected different error message: %v", message)
+	}
+
+}
+
 func TestCreateSessionHandler(t *testing.T) {
-	req, err := http.NewRequest("POST", "/auth/contract/session", nil)
+	sessionRequest := auth.ContractSigningRequest{ Type: "BehandelaarLogin", Language:"NL"}
+	jsonRequest, _ := json.Marshal(sessionRequest)
+	req, err := http.NewRequest("POST", "/auth/contract/session",bytes.NewBuffer(jsonRequest))
 
 	if err != nil {
 		t.Fatal(err)
@@ -78,30 +122,44 @@ func TestGetContract(t *testing.T) {
 	ts := httptest.NewServer(router)
 	defer ts.Close()
 
-	resp, err := http.Get(ts.URL + "/contract/BehandelaarLogin")
-	if err != nil {
-		t.Fatal(err)
-	}
+
+	resp, body := testRequest(t, ts, "GET", "/contract/BehandelaarLogin", nil)
 
 	status := resp.StatusCode
 	if status != http.StatusOK {
 		t.Errorf("Handler returned the wrong status: got %v, expected %v", status, http.StatusOK)
 	}
 
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-
 	var contract auth.Contract
 
-	fmt.Printf("body: %v", string(respBody))
-	if err := json.Unmarshal(respBody, &contract); err != nil {
+	if err := json.Unmarshal([]byte(body), &contract); err != nil {
 		t.Error("Could not unmarshal Contract response", err)
 	}
 
 	if contract.Type != "BehandelaarLogin" {
 		t.Errorf("Wrong kind of contract type: got %v, expected %v", contract.Type, "BehandelaarLogin")
 	}
+}
+
+func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, body)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+	defer resp.Body.Close()
+
+	return resp, string(respBody)
 }
