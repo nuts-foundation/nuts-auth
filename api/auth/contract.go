79a84ba718a96977e1ea51269220c4d21c0e858c
stevenvegt <steven.vandervegt@nedap.com>
2019-04-18 14:05:43 +0200

    Add basic param extraction

--- api/auth/contract.go
+++ api/auth/contract.go
@@ -2,7 +2,9 @@ package auth
 
 import (
 	"github.com/cbroglie/mustache"
+	"github.com/go-errors/errors"
 	"github.com/sirupsen/logrus"
+	"regexp"
 )
 
 type Contract struct {
@@ -12,6 +14,7 @@ type Contract struct {
 	SignerAttributes   []string `json:"signer_attributes"`
 	Template           string   `json:"template"`
 	TemplateAttributes []string `json:"template_attributes"`
+	Regexp             *regexp.Regexp
 }
 
 type ContractSigningRequest struct {
@@ -25,6 +28,7 @@ func ContractByType(contractType string, language string) *Contract {
 
 	switch contractType {
 	case "PractitionerLogin", "BehandelaarLogin":
+		r, _ := regexp.Compile(`NL:BehandelaarLogin:v1 Ondergetekende geeft toestemming aan (.+) om uit zijn/haar naam het nuts netwerk te bevragen. Deze toestemming is geldig van (.+) tot (.+).`)
 		return &Contract{
 			Type:               contractType,
 			Version:            "v1",
@@ -32,6 +36,7 @@ func ContractByType(contractType string, language string) *Contract {
 			SignerAttributes:   []string{"irma-demo.nuts.agb.agbcode"},
 			Template:           `NL:BehandelaarLogin:v1 Ondergetekende geeft toestemming aan {{acting_party}} om uit zijn/haar naam het nuts netwerk te bevragen. Deze toestemming is geldig van {{valid_from}} tot {{valid_to}}.`,
 			TemplateAttributes: []string{"acting_party", "valid_from", "valid_to"},
+			Regexp:             r,
 		}
 	}
 
@@ -42,4 +47,20 @@ func ContractByType(contractType string, language string) *Contract {
 
 func (c Contract) RenderTemplate(vars map[string]string) (string, error) {
 	return mustache.Render(c.Template, vars)
+}
+
+func (c Contract) ExtractParams(text string) ([]string, error) {
+	matchResult := c.Regexp.FindSubmatch([]byte(text))
+	if len(matchResult) < 1 {
+		return nil, errors.New("Could not match the text")
+	}
+	matches := matchResult[1:]
+
+	result := make([]string, len(matches))
+
+	for i, m := range matches {
+		result[i] = string(m)
+	}
+
+	return result, nil
 }
