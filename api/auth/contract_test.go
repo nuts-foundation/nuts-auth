96e82755672eb4468fdc042898e79180f7574230
stevenvegt <steven.vandervegt@nedap.com>
2019-04-18 14:05:43 +0200

    Add basic param extraction

--- api/auth/contract_test.go
+++ api/auth/contract_test.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"reflect"
+	"regexp"
 	"testing"
 )
 
@@ -31,4 +33,40 @@ func TestContract_RenderTemplate(t *testing.T) {
 	if result != expected {
 		t.Errorf("Error while rendering the template: got '%v', expected '%v'", result, expected)
 	}
+}
+
+func TestContract_ExtractParams(t *testing.T) {
+	r, _ := regexp.Compile(`^ik geef toestemming voor (.*) aan (.*)\.$`)
+	contract := &Contract{Type: "Simple", Template: "ik geef toestemming voor {{voor}} aan {{wie}}.", Regexp: r}
+
+	t.Run("a good text returns the params", func(t *testing.T) {
+		contractText := "ik geef toestemming voor orgaandonatie aan Henk."
+
+		expectedParams := []string{"orgaandonatie", "Henk"}
+
+		params, err := contract.ExtractParams(contractText)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !reflect.DeepEqual(expectedParams, params) {
+			t.Errorf("expected different params, expected %v, got %v", expectedParams, params)
+		}
+	})
+
+	t.Run("an invalid text returns an error", func(t *testing.T) {
+		r, _ := regexp.Compile(`^ik geef toestemming voor (.*) aan (.*)\.$`)
+		contract := &Contract{Type: "Simple", Template: "ik geef toestemming voor {{voor}} aan {{wie}}.", Regexp: r}
+
+		contractText := "ik ga niet akkoord."
+
+		params, err := contract.ExtractParams(contractText)
+		if err == nil {
+			t.Error("expected error on invalid contract text")
+		}
+
+		if len(params) != 0 {
+			t.Errorf("expected empty params, got %v", params)
+		}
+	})
 }
