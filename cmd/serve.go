8b70048bebad3a69e71c608226446eee6e337506
stevenvegt <steven.vandervegt@nedap.com>
2019-04-24 16:40:00 +0200

    Use irma base url from config

--- cmd/serve.go
+++ cmd/serve.go
@@ -5,6 +5,7 @@ import (
 	"github.com/nuts-foundation/nuts-proxy/configuration"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
@@ -28,8 +29,12 @@ var serveCmd = &cobra.Command{
 		if err != nil {
 			logrus.WithError(err).Panicf("Could not get config instance")
 		}
+		httpBaseUrl, err := url.Parse(appConfig.HttpAddress);
+		if err != nil {
+			logrus.Panic("Could not parse http address from config. Make sure it is a valid URL")
+		}
 
-		apiConfig := &api.Config{Port: appConfig.HttpPort, Logger: logrus.StandardLogger()}
+		apiConfig := &api.Config{Port: appConfig.HttpPort, Logger: logrus.StandardLogger(), BaseUrl: httpBaseUrl}
 		api := api.New(apiConfig)
 
 		go func() {
