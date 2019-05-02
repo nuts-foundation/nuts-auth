b10ffedbe08b236670139eb07caef0986e65356b
stevenvegt <steven.vandervegt@nedap.com>
2019-05-02 10:23:20 +0200

    Add more tests for contract validation

--- auth/irmacontract.go
+++ auth/irmacontract.go
@@ -53,15 +53,68 @@ func (sc *SignedIrmaContract) verifySignature() (*ValidationResponse, error) {
 }
 
 func (sc *SignedIrmaContract) Validate(actingPartyCn string) (*ValidationResponse, error) {
+	// Verify signature:
 	verifiedContract, err := sc.verifySignature()
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO:
 	// Parse Nuts contract
+	contractMessage := sc.IrmaContract.Message
+	contract := ContractFromMessageContents(contractMessage)
+	if contract == nil {
+		return nil, errors.New("could not find contract")
+	}
+	params, err := contract.ExtractParams(contractMessage)
+	if err != nil {
+		return nil, err
+	}
+
 	// Validate timeframe
+	ok, err := contract.ValidateTimeFrame(params)
+	if ok == false || err != nil {
+		verifiedContract.ValidationResult = Invalid
+		return verifiedContract, err
+	}
+
 	// Validate ActingParty Common Name
+	ok, err = validateActingParty(params, actingPartyCn)
+	if err != nil {
+		return nil, err
+	}
+	if ok == false {
+		verifiedContract.ValidationResult = Invalid
+		return verifiedContract, nil
+	}
 
 	return verifiedContract, nil
+}
+
+// ValidateActingParty checks if an given actingParty is equal to the actingParty in a contract
+// Usually the given acting party comes from the CN of the client-certificate. This check verifies if the
+// contract was actual created by the acting party. This prevents the reuse of the contract by another party.
+func validateActingParty(params map[string]string, actingParty string) (bool, error) {
+	// If no acting party is given, that is probably an implementation error.
+	if actingParty == "" {
+		logrus.Error("No acting party provided. Origin of contract cannot be checked.")
+		return false, errors.New("actingParty cannot be empty")
+	}
+
+	var (
+		actingPartyFromContract string
+		ok                      bool
+	)
+
+	// if no acting party in the params, error
+	if actingPartyFromContract, ok = params["acting_party"]; !ok {
+		return false, errors.New("no acting party found by contract but one given to verify")
+	}
+
+	// perform the actual check
+	if actingParty == actingPartyFromContract {
+		return true, nil
+	}
+
+	// false by default
+	return false, nil
 }
